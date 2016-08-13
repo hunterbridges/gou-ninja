783bb9f90129819d06c1b7c6b5da822734c1610b
Hunter Bridges <hbridges@gmail.com>
2016-08-12 20:59:52 -0700

    Refactor entry point

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
   // Entry point
-  window.speechSynthesis.onvoiceschanged = function() {
+  var runApp = function() {
     var voices = window.speechSynthesis.getVoices();
     HUD.init();
     HUD.show();
     GouNinja.init();
   };
+
+  if (window.speechSynthesis.onvoiceschanged !== undefined) {
+    window.speechSynthesis.onvoiceschanged = runApp;
+  } else {
+    runApp();
+  }
 });
